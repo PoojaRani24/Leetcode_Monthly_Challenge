a6e41308af4dcae3873e4185e2ff62427d5277b1
Pooja Rani <43533334+PoojaRani24@users.noreply.github.com>
2020-06-08 17:26:06 +0530

    Create Power of 2.cpp

---
+++ Power of 2.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n>0 && floor(log2(n))==ceil(log2(n)))
+            return true;
+        else
+            return false;
+    }
+};
