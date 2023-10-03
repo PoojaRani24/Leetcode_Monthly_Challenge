52ae8b9c7d29689dce86aaf9af7803466277f578
Pooja Rani <43533334+PoojaRani24@users.noreply.github.com>
2023-10-04 00:52:54 +0530

    Time: 0 ms (100%), Space: 7 MB (8.36%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool isValid(string s) {
-        if(s[0]==')' || s[0]==']' || s[0]=='}')
-            return false;
+        // if(s[0]==')' || s[0]==']' || s[0]=='}')
+        //     return false;
         stack<char>st;
         for(int i=0;i<s.size();i++){
             if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
