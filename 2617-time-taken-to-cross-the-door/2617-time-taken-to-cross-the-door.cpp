9c859d7277480512e53ac768cd90dc6d831e4fcb
Pooja Rani <43533334+PoojaRani24@users.noreply.github.com>
2024-08-07 09:14:33 +0530

    Time: 76 ms (76.56%), Space: 82.1 MB (28.21%) - LeetHub

--- 2617-time-taken-to-cross-the-door/2617-time-taken-to-cross-the-door.cpp
+++ 2617-time-taken-to-cross-the-door/2617-time-taken-to-cross-the-door.cpp
@@ -1,43 +1,49 @@
 class Solution {
 public:
     vector<int> timeTaken(vector<int>& arrival, vector<int>& state) {
-        int sz = arrival.size(), t = 0, door = 0;
-        vector<int> res(sz, 0);
-        queue<pair<int,int>> enterQ, exitQ;
-
-        for (int i = 0; i < sz; i++) {
-            if (state[i])
-                exitQ.push({arrival[i], i});
+        queue<pair<int,int>>enter,exit;
+        int n=arrival.size();
+        for(int i=0;i<n;i++){
+            if(state[i]==1)
+                exit.push({arrival[i],i});
             else
-                enterQ.push({arrival[i], i});                       
+                enter.push({arrival[i],i});
         }
 
-        while (!exitQ.empty() || !enterQ.empty()) {
-            if ((exitQ.empty() || exitQ.front().first > t) && (enterQ.empty() || enterQ.front().first > t))
-                door = 0;
-            else if (!exitQ.empty() && exitQ.front().first <= t && (enterQ.empty() || enterQ.front().first > t)) {
-                res[exitQ.front().second] = t;
-                door = 2;
-                exitQ.pop();            
+        int time=0, s=0;
+        vector<int>ans(n,0);
+        while(!enter.empty() || !exit.empty()){
+            if((enter.empty() || enter.front().first>time) && (exit.empty() || exit.front().first>time)){
+                time = min(enter.empty() ? INT_MAX : enter.front().first, exit.empty() ? INT_MAX : exit.front().first);
+                s=0;
+                // time++;
             }
-            else if ((exitQ.empty() || exitQ.front().first > t) && !enterQ.empty() && enterQ.front().first <= t) {
-                res[enterQ.front().second] = t;
-                door = 1;
-                enterQ.pop();                
+            else if(!enter.empty() && enter.front().first<=time && (exit.empty() || exit.front().first>time)){
+                ans[enter.front().second]=time;
+                s=1;
+                enter.pop();
+                time++;
             }
-            else {
-                if (door == 0 || door == 2) {
-                    res[exitQ.front().second] = t;
-                    door = 2;
-                    exitQ.pop(); 
-                } else {
-                    res[enterQ.front().second] = t;
-                    door = 1;
-                    enterQ.pop();  
+            else if(!exit.empty() && exit.front().first<=time && (enter.empty() || enter.front().first>time)){
+                ans[exit.front().second]=time;
+                s=2;
+                exit.pop();
+                time++;
+            }
+            else{
+                if(s==1){
+                    ans[enter.front().second]=time;
+                    enter.pop();
+                    s=1;
                 }
+                else{
+                    ans[exit.front().second]=time;
+                    exit.pop();
+                    s=2;
+                }
+                time++;
             }
-            t++;
         }
-        return res;
+        return ans;
     }
 };
