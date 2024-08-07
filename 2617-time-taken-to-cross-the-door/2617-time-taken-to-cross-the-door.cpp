719a36b46795e4091cf1033dd3eeb0ee1ba47af2
Pooja Rani <43533334+PoojaRani24@users.noreply.github.com>
2024-08-07 09:27:25 +0530

    Time: 81 ms (61.9%), Space: 82.1 MB (28.21%) - LeetHub

--- 2617-time-taken-to-cross-the-door/2617-time-taken-to-cross-the-door.cpp
+++ 2617-time-taken-to-cross-the-door/2617-time-taken-to-cross-the-door.cpp
@@ -10,27 +10,27 @@ public:
                 enter.push({arrival[i],i});
         }
 
-        int time=0, s=2;
+        int time=0, s=1;
         vector<int>ans(n,0);
         while(!enter.empty() || !exit.empty()){
             if((enter.empty() || enter.front().first>time) && (exit.empty() || exit.front().first>time)){
                 time = min(enter.empty() ? INT_MAX : enter.front().first, exit.empty() ? INT_MAX : exit.front().first);
-                s=0;
+                s=1;
             }
             else if(!enter.empty() && enter.front().first<=time && (exit.empty() || exit.front().first>time)){
                 ans[enter.front().second]=time;
-                s=1;
+                s=0;
                 enter.pop();
                 time++;
             }
             else if(!exit.empty() && exit.front().first<=time && (enter.empty() || enter.front().first>time)){
                 ans[exit.front().second]=time;
-                s=2;
+                s=1;
                 exit.pop();
                 time++;
             }
             else{
-                if(s==1){
+                if(s==0){
                     ans[enter.front().second]=time;
                     enter.pop();
                 }
