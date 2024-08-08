612519f852f50ba0363231150afd0ec53ebc4b48
Pooja Rani <43533334+PoojaRani24@users.noreply.github.com>
2024-08-08 09:59:07 +0530

    Time: 71 ms (5.68%), Space: 29.5 MB (47.16%) - LeetHub

--- 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.cpp
+++ 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int numberOfPoints(vector<vector<int>>& nums) {
-        sort(nums.begin(),nums.end());
-        int l=nums[0][0];
-        int r=nums[0][1];
-        int len=0;
-        for(int i=1;i<nums.size();i++){
-            if(nums[i][0]>r){
-                len+=(r-l+1);
-                l=nums[i][0];
-                r=nums[i][1];
-            }
-            else{
-                l=min(l,nums[i][0]);
-                r=max(r,nums[i][1]);
-            }
+        vector<int>points(102,0);
+        for(int i=0;i<nums.size();i++){
+            points[nums[i][0]]+=1;
+            points[nums[i][1]+1]+=-1;
         }
-        len+=(r-l+1);
+        int sum=0,len=0;
+        for(int i=1;i<=100;i++){
+            sum+=points[i];
+            cout<<i<<" "<<points[i]<<" "<<sum<<endl;
+            if(sum>0)
+                len++;
+        }
         return len;
     }
 };
+
+// 1 3 4 5 6 7
+// 1 1 1 -1 -1 -1
