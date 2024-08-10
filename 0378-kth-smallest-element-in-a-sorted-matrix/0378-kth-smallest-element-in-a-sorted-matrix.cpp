a7211143426c0a03d53ea87add78c388d40af6c3
Pooja Rani <43533334+PoojaRani24@users.noreply.github.com>
2024-08-10 20:31:01 +0530

    Time: 25 ms (60.45%), Space: 18.1 MB (30.24%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        return kthSmallestMinHeap(matrix,k);
+        return kthSmallestMaxHeap(matrix,k);
     }
 
+    // TC: x=min(r,k) O(x+klogx)
+    // SC: O(x)
     int kthSmallestMinHeap(vector<vector<int>>& matrix, int k) {
         int r=matrix.size();
         int c=matrix[0].size();
@@ -21,5 +23,22 @@ public:
                 pq.push({matrix[row][col+1],{row,col+1}});
         }
         return popedElement;
+    }
+
+    int kthSmallestMaxHeap(vector<vector<int>>& matrix, int k) {
+        int r=matrix.size();
+        int c=matrix[0].size();
+        priority_queue<int>pq;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(pq.size()<k)
+                    pq.push(matrix[i][j]);
+                else if(matrix[i][j]<pq.top()){
+                    pq.pop();
+                    pq.push(matrix[i][j]);
+                }
+            }
+        }
+        return pq.top();
     }
 };
