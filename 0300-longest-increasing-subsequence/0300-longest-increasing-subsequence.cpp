0f7026b4df163c593affa6e9565413871b21b664
Pooja Rani <43533334+PoojaRani24@users.noreply.github.com>
2024-08-12 04:05:40 +0530

    Time: 716 ms (5%), Space: 404 MB (7.16%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<int>lis;
+        vector<vector<int>>dp(2501,vector<int>(2501,-1));
+        return lengthOfLISUtil(nums,0,-1,dp);
+    }
+    int lengthOfLISUtil(vector<int>& nums,int curInd, int prevInd, vector<vector<int>>&dp){
         int n=nums.size();
-        int cnt=0;
-        for(auto num:nums){
-            if(lis.empty()|| num>lis[lis.size()-1]){
-                lis.push_back(num);
-                cnt++;
-            }
-            else{
-                int index=lower_bound(lis.begin(),lis.end(),num)-lis.begin();
-                lis[index]=num;
-            }
-        }
-        return cnt;
+        if(curInd==n)
+            return 0;
+        if(dp[curInd][prevInd+1]!=-1)
+            return dp[curInd][prevInd+1];
+        int canPick = 0;
+        if(prevInd==-1 || nums[curInd]>nums[prevInd])
+            canPick = 1 + lengthOfLISUtil(nums,curInd+1,curInd,dp);
+        int dontPick = lengthOfLISUtil(nums,curInd+1,prevInd,dp);
+        return dp[curInd][prevInd+1] = max(canPick, dontPick);
     }
 };
