c0d2613b7e3e14b55e7676d98ac83a666ada7449
Pooja Rani <43533334+PoojaRani24@users.noreply.github.com>
2024-08-15 18:43:41 +0530

    Time: 193 ms (7.52%), Space: 67.1 MB (5.26%) - LeetHub

---
+++ 1165-meeting-scheduler/1165-meeting-scheduler.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void preprocessAvailability(vector<vector<int>>& slots,map<int,int>&m){
+        for(auto it:slots){
+            m[it[0]]++;
+            m[it[1]+1]--;
+        }
+    }
+
+    vector<int> minAvailableDuration(vector<vector<int>>& slots1, vector<vector<int>>& slots2, int duration) {
+        map<int,int>m;
+        preprocessAvailability(slots1,m);
+        preprocessAvailability(slots2,m);
+        int currentAvailabilityCnt=0, prevAvailabilityCnt=0,currentStartTime=-1,prevStartTime=-1;
+        for(auto it:m){
+            currentStartTime=it.first;
+            currentAvailabilityCnt+=it.second;
+            if(prevStartTime!=-1 && currentAvailabilityCnt<2 && prevAvailabilityCnt==2 && currentStartTime-prevStartTime-1>=duration)
+                return {prevStartTime, prevStartTime+duration};
+            prevStartTime=currentStartTime;
+            prevAvailabilityCnt=currentAvailabilityCnt;
+        }
+        return {};
+    }
+};
