2ee5be07121765cdc2ad038b0dd3e28337e4c6a6
Pooja Rani <43533334+PoojaRani24@users.noreply.github.com>
2024-08-15 18:04:35 +0530

    Time: 78 ms (32.67%), Space: 42.1 MB (57.43%) - LeetHub

---
+++ 1200-remove-interval/1200-remove-interval.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> removeInterval(vector<vector<int>>& intervals, vector<int>& toBeRemoved) {
+        // bool removed=false;
+        int n=intervals.size();
+        if(n==0)
+            return {{}};
+        int i=0;
+        vector<vector<int>>ans;
+        while(i<n){
+            if(intervals[i][1]<toBeRemoved[0] || intervals[i][0]>toBeRemoved[1]){
+                ans.push_back(intervals[i]);
+                i++;
+            }
+            else{
+                if(intervals[i][0]<toBeRemoved[0])
+                    ans.push_back({intervals[i][0],toBeRemoved[0]});
+                if(intervals[i][1]>toBeRemoved[1])
+                    ans.push_back({toBeRemoved[1],intervals[i][1]});
+                // removed=true;
+                i++;
+            }
+        }
+        return ans;
+    }
+};
