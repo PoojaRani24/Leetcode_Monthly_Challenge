f835be7dfee118dacea58c0da4e6ef5f8179f61c
Pooja Rani <43533334+PoojaRani24@users.noreply.github.com>
2024-08-16 22:33:20 +0530

    Time: 6 ms (98.81%), Space: 15.1 MB (30.49%) - LeetHub

--- 1222-remove-covered-intervals/1222-remove-covered-intervals.cpp
+++ 1222-remove-covered-intervals/1222-remove-covered-intervals.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
         auto cmp=[&](const vector<int>&a, const vector<int>&b){
-            if(a[0]==b[0] && a[1]>b[1])
-                return true;
+            if(a[0]==b[0])
+                return a[1]>b[1];
             return a[0]<b[0];
         };
+
         int n=intervals.size();
         sort(intervals.begin(),intervals.end(),cmp);
         int cnt=n, end=intervals[0][1];
