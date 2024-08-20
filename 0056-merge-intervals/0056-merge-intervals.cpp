86ba32143a07f205668c47fcd42b8942eac83d85
Pooja Rani <43533334+PoojaRani24@users.noreply.github.com>
2024-08-21 01:07:28 +0530

    Time: 25 ms (45.72%), Space: 23 MB (32.14%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,9 +3,8 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
         vector<vector<int>>ans;
-        ans.push_back(intervals[0]);
-        for(int i=1;i<intervals.size();i++){
-            if(ans.back()[1]<intervals[i][0])
+        for(int i=0;i<intervals.size();i++){
+            if(ans.empty() || ans.back()[1]<intervals[i][0])
                 ans.push_back(intervals[i]);
             else
                 ans.back()[1]=max(ans.back()[1],intervals[i][1]);
