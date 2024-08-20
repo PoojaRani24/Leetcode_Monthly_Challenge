746792960c83d27b27d8f55399f79e44003d2ace
Pooja Rani <43533334+PoojaRani24@users.noreply.github.com>
2024-08-21 00:56:27 +0530

    Time: 208 ms (70.79%), Space: 74.7 MB (60.67%) - LeetHub

--- 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     int maxEvents(vector<vector<int>>& events) {
-        int n=events.size();
-        auto cmp=[&](vector<int>&a,vector<int>&b){
-            if(a[0]==b[0])
-                return a[1]<b[1];
-            return a[0]<b[0];
-        };
-        sort(events.begin(),events.end(),cmp);
-        int cnt=0;
-        int d=1;
+        sort(events.begin(),events.end());
+        int n=events.size(), d=1, i=0, cnt=0;
         priority_queue<int,vector<int>,greater<int>>pq;
-        int i=0;
         while(!pq.empty() || i<n){
             if(pq.empty())
                 d=events[i][0];
             while(i<n && events[i][0]==d){
                 pq.push(events[i][1]);
-                i++;}
-            pq.pop();
-            cnt++;
+                i++;
+            }
             d++;
+            cnt++;
+            pq.pop();
             while(!pq.empty() && pq.top()<d)
                 pq.pop();
         }
