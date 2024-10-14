8b76f92125e6b088f0d4bacb70d21a6899bc2502
Pooja Rani <43533334+PoojaRani24@users.noreply.github.com>
2024-10-14 21:22:59 +0530

    Time: 26 ms (55.03%), Space: 20.8 MB (40.34%) - LeetHub

--- 0690-employee-importance/0690-employee-importance.cpp
+++ 0690-employee-importance/0690-employee-importance.cpp
@@ -12,17 +12,15 @@ class Solution {
 public:
     int getImportance(vector<Employee*> employees, int id) {
         unordered_map<int,pair<int,vector<int>>>m;
-        for(int i=0;i<employees.size();i++){
-            m[employees[i]->id]={employees[i]->importance, employees[i]->subordinates};
-        }
+        for(auto it: employees)
+            m[it->id]={it->importance, it->subordinates};
         return resolve(id,m);
     }
 
     int resolve(int id, unordered_map<int,pair<int,vector<int>>>&m){
        int val = m[id].first;
-       for(int i=0;i<m[id].second.size();i++){
-            val+=resolve(m[id].second[i],m);
-       }
+       for(auto it:m[id].second)
+            val+=resolve(it,m);
        return val;
     }
 };
