9b82888bd0cbfc6bc60e956462a702d48f7e820c
Pooja Rani <43533334+PoojaRani24@users.noreply.github.com>
2024-11-03 20:11:32 +0530

    Time: 3 ms (30.65%), Space: 9.1 MB (35.04%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -1,29 +1,18 @@
-#include <stack>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        int maxLen = 0;
-        stack<int> st;
-        st.push(-1);  // Initialize with -1 as a base for the first valid substring
-
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == '(') {
-                // Push index of '(' to stack
+        stack<int>st;
+        st.push(-1);
+        int maxLen=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(')
                 st.push(i);
-            } else {
-                // Pop the last '(' index
+            else{
                 st.pop();
-                
-                if (!st.empty()) {
-                    // Calculate the length of the current valid substring
-                    maxLen = max(maxLen, i - st.top());
-                } else {
-                    // No matching '(', so push current index as a boundary
+                if(!st.empty())
+                    maxLen=max(maxLen,i-st.top());
+                else
                     st.push(i);
-                }
             }
         }
         return maxLen;
