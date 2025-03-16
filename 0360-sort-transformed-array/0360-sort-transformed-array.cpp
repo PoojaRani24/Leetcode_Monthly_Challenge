9e7c8c65d42b43401eca2ff0c163ca81924d3687
Pooja Rani <43533334+PoojaRani24@users.noreply.github.com>
2025-03-16 20:51:04 +0530

    Time: 0 ms (100%), Space: 13.1 MB (19.42%) - LeetHub

--- 0360-sort-transformed-array/0360-sort-transformed-array.cpp
+++ 0360-sort-transformed-array/0360-sort-transformed-array.cpp
@@ -2,10 +2,38 @@ class Solution {
 public:
     vector<int> sortTransformedArray(vector<int>& nums, int a, int b, int c) {
        vector<int>res;
-       for(int i=0;i<nums.size();i++){
-        res.push_back(getValue(nums[i],a,b,c)); 
+       int left=0,right=nums.size()-1;
+       if(a>0){
+        //Upward parabola
+        while(left<=right){
+            int leftVal=getValue(nums[left],a,b,c);
+            int rightVal=getValue(nums[right],a,b,c);
+            if(leftVal>=rightVal){
+                res.push_back(leftVal);
+                left++;
+            }
+            else{
+                res.push_back(rightVal);
+                right--;
+            }
+        }
+        reverse(res.begin(),res.end());
        }
-        sort(res.begin(), res.end());
+       else{
+        //Downward parabola
+        while(left<=right){
+            int leftVal=getValue(nums[left],a,b,c);
+            int rightVal=getValue(nums[right],a,b,c);
+            if(leftVal<=rightVal){
+                res.push_back(leftVal);
+                left++;
+            }
+            else{
+                res.push_back(rightVal);
+                right--;
+            }
+        }
+       }
        return res;
     }
 
