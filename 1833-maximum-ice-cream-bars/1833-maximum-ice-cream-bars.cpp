e83440749b43d2504846e6a50b5e0a7ee9fd58db
Pooja Rani <43533334+PoojaRani24@users.noreply.github.com>
2026-06-21 15:28:31 +0530

    Time: 3 ms (94.33%), Space: 84.3 MB (19.23%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int maxIceCream(vector<int>& costs, int coins) {
+    int maxIceCreamII(vector<int>& costs, int coins) {
         sort(costs.begin(), costs.end());
         int maxIceCreamCnt=0;
         for(int i=0;i<costs.size();i++){
@@ -10,5 +10,23 @@ public:
             }
         }
         return maxIceCreamCnt;
+    }
+
+    int maxIceCream(vector<int>& costs, int coins) {
+        int n = costs.size();
+        int range = 0;
+        for(int i=0;i<n;i++)
+            range = max(range, costs[i]);
+        vector<int>freq(range+1,0);
+        for(int i=0;i<n;i++)
+            freq[costs[i]]++;
+        int ans=0;
+        for(int i=1;i<=range && coins>0;i++){
+            int pick = min(coins/i, freq[i]);
+            ans = ans + pick;
+            coins-=(pick*i);
+        }
+        return ans;
+
     }
 };
