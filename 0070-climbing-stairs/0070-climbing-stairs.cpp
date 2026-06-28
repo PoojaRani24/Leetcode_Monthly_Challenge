c3b791550f54d82437d1210521493bb6704f36d1
Pooja Rani <43533334+PoojaRani24@users.noreply.github.com>
2026-06-28 17:52:56 +0530

    Time: 0 ms (100%), Space: 7.9 MB (71.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,11 +3,14 @@ public:
     int climbStairs(int n) {
         if(n==1)
             return 1;
-        vector<int>dp(n+1,1);
-        dp[1]=1;
-        dp[2]=2;
-        for(int i=3;i<=n;i++)
-            dp[i]=dp[i-1]+dp[i-2];
-        return dp[n];
+        int first=1;
+        int second=2;
+        int current=0;
+        for(int i=3;i<=n;i++){
+            current=first+second;
+            first=second;
+            second=current;
+        }
+        return second;
     }
 };
