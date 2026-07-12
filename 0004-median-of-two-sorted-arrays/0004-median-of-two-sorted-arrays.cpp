984baa899f20ff116cc0736c9fa2ba07a3e21f26
Pooja Rani <43533334+PoojaRani24@users.noreply.github.com>
2026-07-12 14:03:11 +0530

    Time: 0 ms (100%), Space: 95.2 MB (61.77%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -6,25 +6,25 @@ public:
         if(n1>n2)
             return findMedianSortedArrays(nums2, nums1);
         int low=0, high=n1;
-        int total = (n1+n2+1)/2;
+        int totalLeft = (n1+n2+1)/2;
         while(low<=high){
-            int mid = low+(high-low)/2;
-            int left = total-mid;
-            int l1 = mid-1 >= 0 ? nums1[mid-1]:INT_MIN;
-            int l2 = left-1 >= 0 ? nums2[left-1]:INT_MIN;
-            int r1 = mid<n1 ? nums1[mid]:INT_MAX;
-            int r2 = left<n2 ? nums2[left]:INT_MAX;
-            if(l1<=r2 && l2<=r1)
+            int partition1 = low+(high-low)/2;
+            int partition2 = totalLeft-partition1;
+            int left1 = partition1-1 >= 0 ? nums1[partition1-1]:INT_MIN;
+            int left2 = partition2-1 >= 0 ? nums2[partition2-1]:INT_MIN;
+            int right1 = partition1<n1 ? nums1[partition1]:INT_MAX;
+            int right2 = partition2<n2 ? nums2[partition2]:INT_MAX;
+            if(left1<=right2 && left2<=right1)
                 {
                    if((n1+n2)%2==0)
-                        return (double)(max(l1,l2)+min(r1,r2))/2.0; 
+                        return (double)(max(left1,left2)+min(right1,right2))/2.0; 
                     else
-                        return max(l1,l2);
+                        return max(left1,left2);
                 }
-            else if(l1>r2)
-                high=mid-1;
-            else if(l2>r1)
-                low=mid+1;
+            else if(left1>right2)
+                high=partition1-1;
+            else if(left2>right1)
+                low=partition1+1;
         }
         return 0;
     }
