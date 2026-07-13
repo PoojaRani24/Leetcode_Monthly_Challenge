e0202a89d4d068790eefa90db5ffefb16eee6796
Pooja Rani <43533334+PoojaRani24@users.noreply.github.com>
2026-07-13 23:54:50 +0530

    Time: 3 ms (75.34%), Space: 13.4 MB (18.02%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -5,13 +5,14 @@ public:
             {'I',1}, {'V',5},{'X',10}, {'L',50}, {'C',100}, {'D',500}, {'M',1000}
         };
         int ans=0;
-        for(int i=0;i<s.size()-1;i++){
+        int n= s.size();
+        for(int i=0;i<n-1;i++){
             if(m[s[i]]<m[s[i+1]])
                 ans-=m[s[i]];
             else
                 ans+=m[s[i]];
         }
-        ans+=m[s[s.size()-1]];
+        ans+=m[s[n-1]];
         return ans;
     }
 };
