0308f59ceb1d330a08efcbe4f194410442348789
Pooja Rani <43533334+PoojaRani24@users.noreply.github.com>
2026-07-15 00:54:02 +0530

    Time: 0 ms (100%), Space: 10.5 MB (42.69%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,37 +1,39 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
 class Solution {
 public:
     int splitArray(vector<int>& nums, int k) {
-        int low = *max_element(nums.begin(),nums.end());
-        int high = accumulate(nums.begin(),nums.end(),0);
-        int res=low;
-        //------------logic--------------------------------
-        while(low<=high){
-            int mid = (low+high)/2;
-            int n = no_of_subarrays_with_max_sum_as_mid(nums,mid);
-            if(n > k)
-                low = mid+1;
-            else{
-                res = mid;
-                high = mid-1;
+        int low = 0, high = 0;
+        for (int num : nums) {
+            low = max(low, num);
+            high += num;
+        }
+        int ans = high;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (isFeasible(nums, mid, k)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
-        //-------------------------------------------------
-        return res;
+        return ans;
     }
-
-    int no_of_subarrays_with_max_sum_as_mid(vector<int>& nums, int mid){
-        int cnt=1;
-        int sum=0;
-        for(int i=0;i<nums.size();i++){
-            if(sum+nums[i] > mid){
-                cnt++;
-                sum=nums[i];
-            }
-            else{
-                sum+=nums[i];
+    
+    bool isFeasible(vector<int>& nums, int maxSum, int k) {
+        int count = 1;
+        int currentSum = 0;
+        for (int num : nums) {
+            currentSum += num;
+            if (currentSum > maxSum) {
+                count++;
+                currentSum = num;
+                if (count > k) return false;
             }
         }
-        return cnt;
+        return true;
     }
-
 };
