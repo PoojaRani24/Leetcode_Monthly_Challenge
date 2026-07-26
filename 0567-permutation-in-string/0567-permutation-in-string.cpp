4216ff5fde33866f0376353118db7b3c322cd1ea
Pooja Rani <43533334+PoojaRani24@users.noreply.github.com>
2026-07-26 13:29:53 +0530

    Time: 4 ms (52.45%), Space: 9.8 MB (89.66%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -2,33 +2,24 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         int n1=s1.size();
-        vector<int>m1(26,0);
-        for(int i=0;i<n1;i++)
-            m1[s1[i]-'a']++;
-        int left=0;
         int n2=s2.size();
         if (n1 > n2)
             return false;
-        vector<int>m2(26,0);
+
+        vector<int>m1(26,0), m2(26,0);
+        for(int i=0;i<n1;i++)
+            m1[s1[i]-'a']++;
+
+        int left=0;
         for(int right=0;right<n2;right++){
             m2[s2[right]-'a']++;
             while(right-left+1 > n1){
                 m2[s2[left]-'a']--;
-                // if(m2[s2[left]-'a']==0)
-                //     m2.erase(s2[left]);
                 left++;
             }
-            if(right-left+1 == n1 && window_valid(m1,m2))
+            if(right-left+1 == n1 && m1==m2)
                 return true;
         }
         return false;
-    }
-
-    bool window_valid(vector<int>&m1, vector<int>&m2){
-        for(int i=0;i<26;i++){
-            if(m1[i]!=m2[i])
-                return false;
-        }
-        return true;
     }
 };
